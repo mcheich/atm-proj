dc9ab7597bf3dbe43716f30bbdc64031f80a7a17
mcheich <mcheich@gmail.com>
2020-01-11 10:07:44 -0500

    Added getBalance test and method

--- src/main/java/jUnitTests/Atm.java
+++ src/main/java/jUnitTests/Atm.java
@@ -2,15 +2,25 @@ package jUnitTests;
 
 public class Atm {
 	
-	private String pin = "1234";
+	private String requiredPin;
+	private double balance = 297.00;
+
+	public Atm(String pin) {
+		requiredPin = pin;
+	}
 
 	public boolean allowAcess(String pinEntered) {
 		
-		if(pinEntered.equals(pin)) {
+		if(pinEntered.equals(requiredPin)) {
 		return true;
 		}
 		
 		return false;
+	}
+
+	public double getBalance() {
+	
+		return balance;
 	}
 
 }
--- src/test/java/jUnitTests/ATMUnitTests.java
+++ src/test/java/jUnitTests/ATMUnitTests.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ATMUnitTests {
 
 	//Arrange
-	Atm underTest = new Atm();
+	Atm underTest = new Atm("1234");
 	
 	@Test
 	public void allowAccessTest() {
@@ -19,11 +19,20 @@ public class ATMUnitTests {
 	
 	@Test
 	public void denyAccessTest() {
-	
 		//Act
 		boolean result = underTest.allowAcess("1010");
 		//Assert
 		assertEquals(false, result);
 	}
+	
+	@Test
+	public void getBalanceTest() {
+		//Act
+		double result = underTest.getBalance();
+		//Assert
+		assertTrue(297.00 == result);	
+	}
+	
+	
 
 }
